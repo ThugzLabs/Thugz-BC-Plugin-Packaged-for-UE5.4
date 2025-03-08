b9c8e5da0934d222347fa755761d04e28f267bf4
Tchiki <122636431+TchikiBalianos@users.noreply.github.com>
2025-03-08 05:36:58 +0100

    Update ThugzBCfor53.Build.cs
    Add a commented copyright notice with Publisher name and year of publishing.

--- Source/ThugzBCfor53/ThugzBCfor53.Build.cs
+++ Source/ThugzBCfor53/ThugzBCfor53.Build.cs
@@ -1,5 +1,16 @@
-// Publisher Name: Thugz Labs
-// 2025
+// Copyright (c) 2025 Thugz Labs
+// All Rights Reserved.
+//
+// This software is provided "as-is," without any express or implied warranty.
+// In no event shall the authors or copyright holders be held liable for any damages 
+// arising from the use of this software.
+//
+// You are permitted to use, modify, and distribute this software under the terms 
+// of the license specified in the LICENSE file included with this repository.
+//
+// Publisher: Thugz Labs
+// Year of Publication: 2025
+
 
 using System;
 using System.IO;
